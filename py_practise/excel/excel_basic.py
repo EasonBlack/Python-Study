4d73e9010ac76a036c1eaa83afe0468697dd2422
easonblack <eason820720@163.com>
2017-08-21 15:49:18 +0800

    up

--- py_practise/excel/excel_basic.py
+++ py_practise/excel/excel_basic.py
@@ -3,8 +3,7 @@ import numpy as np
 df = pd.read_excel("demo.xlsx", 'Sheet2')
 print df.keys()
 dt = df.sort_values(['team'])#[['name','team']] 
-dt = dt.where(dt['team'] == 2).drop_duplicates(['name','team'], keep=False)
+dt = dt.query("team==1").head()
+#dt = dt.where(dt['team'] == 2).drop_duplicates(['name','team'])
 dt = dt.append(dt.count(), ignore_index=True)
 print dt
-
-#duplicated
