a6a6ceeda4c5a9bcf65536055b94732368c14f33
easonblack <eason820720@163.com>
2018-01-03 14:16:32 +0800

    up

--- py_modules_basic/configparser/basic/app.py
+++ py_modules_basic/configparser/basic/app.py
@@ -9,6 +9,15 @@ print(cf.sections())
 for c in cf.sections():
     print cf.options(c)    
 
+aa = cf.get('test', 'aa')
+print(json.loads(aa))
+
+bb = cf.get('test', 'bb')
+print(json.loads(bb)[0])
+
 cc = cf.get('test', 'cc')
 print(json.loads(cc))
 print(json.loads(cc)['x'])
+
+dd = cf.get('test', 'dd')
+print(dd)
