7b71d6daa4be4a33d2bcc30b9f5fbe49637d102e
EasonBlack <eason820720@163.com>
2018-01-16 21:31:19 +0800

    up

--- py_tools/auto_deploy/demo/fabric-demo.py
+++ py_tools/auto_deploy/demo/fabric-demo.py
@@ -3,12 +3,31 @@
 from fabric.api import *
 
 env.user = 'root'
-env.hosts=['xx.xx.xx.xx'] 
-env.password = 'pw'
+env.hosts=['47.52.102.168'] 
+
 
 
 def play():   
-    print "Hello World"
-    with cd('/'):
-        run("ls -l")
-   
+    print("Hello World")
+    print(env.hosts[0])
+    
+
+def connect():
+    run('pwd')
+    with cd('/usr/share/nginx/html/campaign'):
+        run("ls")
+
+list = [
+    "-r e:/alead/campaign/dist",
+    "e:/alead/campaign/index.html"
+]
+dest = "root@" + env.hosts[0] + ":/usr/share/nginx/html/campaign"
+
+def update():
+    for o in list:
+        local("pscp " + o + " " + dest)
+
+def build():
+    with lcd("e:\Blead\campaign"):
+        local("npm run build")
+
