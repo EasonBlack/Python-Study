93645db9df9af890eec12e567083b53a1f6a4b09
EasonBlack <eason820720@163.com>
2018-09-02 14:45:50 +0800

    up

--- py_tools/auto_structure_by_txt/app.py
+++ py_tools/auto_structure_by_txt/app.py
@@ -10,19 +10,27 @@ lines = my_file.readlines()
 
 
 tabRegex =re.compile(r"(\s{2,4})")
+templateRegex = re.compile(r"\[(\S+)\]")
 root = './dist'
+templateFolder = './templates'
 current = './dist'
 currentParent = ''
 currentTab = 0
 
 if os.path.exists(root):
-    shutil.rmtree(root) 
+    shutil.rmtree(root,ignore_errors=True ) 
 os.makedirs(root)
 
 
 for line in lines:
     length=len(tabRegex.findall(line))
+    lineTemp = templateRegex.findall(line)
+    # print(lineTem)
     line= line.replace('\n', '').replace(' ', '')
+
+    if line == '':
+        continue
+
     if length > currentTab:
         currentTab = length
         current = current + '\\' + currentParent
@@ -33,11 +41,16 @@ for line in lines:
     currentParent = line
     
     _target = ''.join([current,'\\',line])
+    _target = templateRegex.sub('', _target)   # 去掉[]
     lineArr = line.split('.')
-    if len(lineArr) > 1:
-        # os.mknod(_target) 
-        _file = open(_target,'w',encoding='utf8')
-        _file.close()
+   
+    if len(lineArr) > 1:       
+        if len(lineTemp) >0:
+            # print(_target, lineTemp[0], len(lineTemp))
+            shutil.copyfile(templateFolder+"\\" + lineTemp[0], _target) 
+        else:
+            _file = open(_target,'w',encoding='utf8')
+            _file.close()
     else:
         os.makedirs(_target)
 
